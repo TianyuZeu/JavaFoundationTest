dd31ada772267a1ab2c707c30a5bb48416b76cf5
zhutianyu <2638879086@qq.com>
2020-11-19 17:33:04 +0800

    除了管道 打印 推回输入都完成了，还有一处bug在stringReader和Stringwriter

--- src/ByteStreamTest.java
+++ src/ByteStreamTest.java
@@ -84,6 +84,46 @@ public class ByteStreamTest {
         }
     }
 
+    private void ObjectIOStream(){
+        //Object I/O Stream
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("E:\\test\\oiostest.txt"));
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("E:\\test\\oiostest.txt"))){
+           Newtouch newtouchOut = new Newtouch("newtouch",26,true);
+           oos.writeObject(newtouchOut);
+           Newtouch newtouchIn = (Newtouch) ois.readObject();
+            System.out.println("读娶到的对象: "+newtouchIn.toString());
+        }catch(IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void ByteArrayIOStream(){
+        //ByteArray I/O Stream
+        byte[] b = {0x6e,0x65,0x77,0x74,0x6f,0x75,0x63,0x68};
+        try(ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ByteArrayInputStream bis = new ByteArrayInputStream(b)){
+
+            byte[] out = new byte[32];
+            bos.write(b,0,8);
+            out = bos.toByteArray();
+            for (int i = 0;i<out.length;i++) {
+                System.out.print((char)out[i]);
+            }
+            bos.flush();
+            System.out.println("\n====风歌线=====");
+            byte[] in = new byte[32];
+            int len = 0;
+            while((len = bis.read(in))!=-1){
+                String str = new String(in,0,len);
+                System.out.println(str);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //piped  thread
+
 
 
 
@@ -93,13 +133,65 @@ public class ByteStreamTest {
         ByteStreamTest bst = new ByteStreamTest();
 
         //访问文件 fileInputStream fileOutputStream
-        bst.FileIOStream();
+        //bst.FileIOStream();
 
         //基本类型的二进制存取 DatInputStream DataInputStream
-        bst.DateIOStream();
+       // bst.DateIOStream();
 
         //缓冲 BufferedInputStream BufferedOutputStream
-        bst.BufferedIOStreamTest();
+        //bst.BufferedIOStreamTest();
+
+        //对象  ObjectIOStream
+        //bst.ObjectIOStream();
+
+        //ByteArray I/O Stream
+        bst.ByteArrayIOStream();
     }
+
+}
+
+
+
+
+class Newtouch implements Serializable {
+    private String s;
+    private int i;
+    private boolean b;
+
+    public Newtouch(String s, int i, boolean b) {
+        this.s = s;
+        this.i = i;
+        this.b = b;
+    }
+
+
+    public String toString(){
+        return this.getS()+"+"+this.getI()+"+"+this.isB();
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public boolean isB() {
+        return b;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+
 
 }
---
+++ src/CharacterStreamTest.java
@@ -0,0 +1,144 @@
+import java.io.*;
+import java.sql.SQLOutput;
+
+public class CharacterStreamTest {
+
+    //访问文件 FileReader FileWriter
+    private void FileRw(){
+
+        FileWriter fw = null;
+        FileReader fr = null;
+        try{
+            fw = new FileWriter("E:\\test\\file.txt");
+            fr = new FileReader("E:\\test\\file.txt");
+            fw.write("file reader writer : newtouchchchchchchchh");
+            fw.flush();
+            char[] c = new char[32];
+            int i = 0;
+            while((i = fr.read(c))!= - 1){
+                String str = new String(c,0,i);
+                System.out.println(str);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }  finally {
+            try {
+                fw.close();
+                fr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //缓冲流 buffered rw
+    private void BufferedRw(){
+
+        BufferedWriter bw = null;
+        BufferedReader br = null;
+        try{
+            bw = new BufferedWriter(new FileWriter("E:\\test\\buf.txt"));
+            br = new BufferedReader(new FileReader("E:\\test\\buf.txt"));
+            bw.write("buffered reader writer : newtouchooooo");
+            bw.flush();
+            char[] c = new char[32];
+            int i = 0;
+            while((i = br.read(c))!= - 1){
+                String str = new String(c,0,i);
+                System.out.println(str);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }  finally {
+            try {
+                bw.close();
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //charArray rw
+    private void charArrayRw(){
+
+        char[] c = {'n','e','w','t','o','u','c','h'};
+        CharArrayWriter cw = null;
+        CharArrayReader cr = null;
+        try{
+            cw = new CharArrayWriter();
+            cw.write(c,0,8);
+            char[] write = cw.toCharArray();
+            String str = new String(write);
+            System.out.println(str);
+
+            cr = new CharArrayReader(c);
+            char[] read  = new char[32];
+            int i = 0;
+            while((i = cr.read(read))!=-1){
+                String str1 = new String(read,0,i);
+                System.out.println(str1);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            cw.close();
+            cr.close();
+        }
+    }
+
+    //StringReader writer
+    private void stringRw(){
+
+        String str = "new touch";
+        StringWriter sw = null;
+        StringReader sr = null;
+        try{
+            sw = new StringWriter();
+            sw.write(str);
+            System.out.println(str);
+
+            sr = new StringReader(str);
+            char[] c = new char[32];
+            int i = 0;
+            while((i = sr.read(c,0,i))!=-1){
+                String str1 = new String(c,0,i);
+                System.out.println(str1);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                sw.close();
+                sr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        CharacterStreamTest characterStreamTest = new CharacterStreamTest();
+
+        //访问文件 FileReader FileWriter
+        //characterStreamTest.FileRw();
+
+        //缓冲流 buffered rw
+        //characterStreamTest.BufferedRw();
+
+        //charArray rw
+        //characterStreamTest.charArrayRw();
+
+        //String rw
+        characterStreamTest.stringRw();
+    }
+}
